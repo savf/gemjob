dad4890e59dd4569504265d50e97f2c0ab4de19c
Stephan Mannhart <stephan.mannhart@googlemail.com>
2017-07-09 17:10:11 +0200

    Removed matplotlib dependency in cluster_module/dm_general

--- cluster_module/dm_general.py
+++ cluster_module/dm_general.py
@@ -1,11 +1,10 @@
-from sklearn.metrics import explained_variance_score, mean_squared_error, mean_absolute_error, accuracy_score
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import scipy.stats as stats
-import seaborn as sns
 import os
-from matplotlib.colors import ListedColormap
+
+import pandas as pd
+import scipy.stats as stats
+from sklearn.metrics import explained_variance_score, mean_squared_error, \
+    mean_absolute_error, accuracy_score
+
 
 def print_data_frame(title, df):
     """ Print stats about a given Pandas DataFrame with a given title
@@ -27,8 +26,7 @@ def print_data_frame(title, df):
     print "############################## \n\n"
 
 
-def print_correlations(df, attr=None, store=False, method='spearman',
-                       xlabels=None, ylabels=None):
+def print_correlations(df, attr=None, method='spearman'):
     """ Print attribute correlations for a given Pandas DataFrame
 
     Spearman and kendall are robust in regards to outliers whereas pearson
@@ -38,67 +36,31 @@ def print_correlations(df, attr=None, store=False, method='spearman',
     :type df: pandas.DataFrame
     :param attr: If specified, only print correlations for the given attribute
     :type attr: str
-    :param store: Whether to store the correlations and significance as CSV
-    :type store: bool
-    :param method: Which correlation method to use: pearson, spearman or kendall
+    :param method: Correlation method to use (spearman, kendall or pearson)
     :type method: str
-    :param xlabels: List with labels for the x-axis tick marks
-    :type xlabels: list(str)
-    :param ylabels: List with labels for the y-axis tick marks
-    :type ylabels: list(str)
     """
     corr = df.corr(method)
-    dropped_columns = list(set(df.columns) - set(corr.columns))
-    df.drop(labels=dropped_columns, axis=1, inplace=True)
-    significance = np.zeros([df.shape[1], df.shape[1]])
 
-    for row in range(df.shape[1]):
-        for column in range(df.shape[1]):
-            row_label = df.columns[row]
-            column_label = df.columns[column]
-            if method == 'pearson':
-                significance[row][column] = stats.pearsonr(df[row_label], df[column_label])[1]
-            elif method == 'kendall':
-                significance[row][column] = stats.kendalltau(df[row_label], df[column_label])[1]
-            else:
-                significance[row][column] = stats.spearmanr(df[row_label], df[column_label])[1]
-
-    corr_significance = pd.DataFrame(significance)
-    corr_significance.columns = df.columns.values
-    corr_significance.set_index(df.columns.values, inplace=True)
-
-    if attr is None:
-        fig1, ax1 = plt.subplots()
-        fig2, ax2 = plt.subplots()
-        plt.subplots_adjust(left=0.1, bottom=0.2)
-        green_to_red = ["#30d43f", "#49ff61", "#ff8787", "#ff4545", "#ff0000"]
-        custom_cmap = ListedColormap(sns.color_palette(green_to_red).as_hex())
-        sns.heatmap(corr, vmax=1.0, square=True, cmap="OrRd", ax=ax1)
-        sns.heatmap(significance, vmax=1.0, square=True, cmap=custom_cmap, ax=ax2)
-        for ax in [ax1, ax2]:
-            ax.set_xticklabels(ax.xaxis.get_majorticklabels(), rotation=90)
-            ax.set_yticklabels(ax.yaxis.get_majorticklabels(), rotation=0)
-        if xlabels is not None:
-            xlabels = [label for label in xlabels if label not in set(dropped_columns)]
-            ax1.set_xticklabels(xlabels)
-            ax2.set_xticklabels(xlabels, rotation=90)
-        if ylabels is not None:
-            ylabels = [label for label in ylabels if label not in set(dropped_columns)]
-            ax1.set_yticklabels(ylabels[::-1])
-            ax2.set_yticklabels(ylabels[::-1], rotation=0)
-        plt.show()
-
-    else:
+    if attr is not None:
         print "### Correlations for " + attr + " ###"
         print corr[attr].abs().sort_values(ascending=False)
+        print "### p-Values for correlations for " + attr + " ###"
+        if method == "spearman":
+            for column in range(df.shape[1]):
+                print "{} and {}: {}".format(attr,
+                                             df.columns[column],
+                                             stats.spearmanr(df[attr], df[df.columns[column]]))
+        elif method == "kendall":
+            for column in range(df.shape[1]):
+                print "{} and {}: {}".format(attr,
+                                             df.columns[column],
+                                             stats.kendalltau(df[attr], df[df.columns[column]]))
+        else:
+            for column in range(df.shape[1]):
+                print "{} and {}: {}".format(attr,
+                                             df.columns[column],
+                                             stats.pearsonr(df[attr], df[df.columns[column]]))
     print "################################ \n\n"
-    if store:
-        with open('correlations.csv', 'w') as f:
-            f.write(corr.to_csv())
-        f.close()
-        with open('correlation_significances.csv', 'w') as f:
-            f.write(corr_significance.to_csv())
-        f.close()
 
 
 def print_statistics(df):
