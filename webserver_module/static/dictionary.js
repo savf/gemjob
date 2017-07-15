7e311c511f9d25621e6248db45416e5357ebfa1a
Stephan Mannhart <stephan.mannhart@googlemail.com>
2017-07-16 01:50:24 +0200

    Added highcharts and dictionary to repo

---
+++ webserver_module/static/dictionary.js
@@ -0,0 +1,74 @@
+var dict = {
+    "budget": {
+        en: "Budget"
+    },
+    "client_country": {
+        en: "Country"
+    },
+    "client_feedback": {
+        en: "Overall feedback"
+    },
+    "client_jobs_posted": {
+        en: "Number of jobs you created"
+    },
+    "client_past_hires": {
+        en: "Number of hires"
+    },
+    "client_payment_verification_status": {
+        en: "Payment verification status"
+    },
+    "client_reviews_count": {
+        en: "Number of reviews you received"
+    },
+    "timestamp": {
+        en: "Start date"
+    },
+    "duration_weeks_median": {
+        en: "Duration in weeks"
+    },
+    "experience_level":  {
+        en: "Desired Experience Level"
+    },
+    "feedback_for_client": {
+        en: "Feedback from freelancers for this job"
+    },
+    "feedback_for_freelancer": {
+        en: "Feedback to freelancers for this job"
+    },
+    "freelancer_count": {
+        en: "Number of freelancers"
+    },
+    "job_type":  {
+        en: "Payment type"
+    },
+    "skills": {
+        en: "Skills"
+    },
+    "skills_number": {
+        en: "Number of skills"
+    },
+    "snippet": {
+        en: "Description"
+    },
+    "snippet_length": {
+        en: "Length of description"
+    },
+    "subcategory2":  {
+        en: "Subcategory"
+    },
+    "title":  {
+        en: "Title"
+    },
+    "title_length": {
+        en: "Length of title"
+    },
+    "total_charge": {
+        en: "Total costs of this job"
+    },
+    "total_hours": {
+        en: "Duration in hours"
+    },
+    "workload":  {
+        en: "Workload"
+    }
+}
