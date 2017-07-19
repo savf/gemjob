3b29e56cd717951a16c250cedde9ecee8acd7c71
bthdimension <benjamin.buergisser@gmx.ch>
2017-07-19 23:38:53 +0200

    k-NN updated

--- dm_lib/dm_knn.py
+++ dm_lib/dm_knn.py
@@ -25,6 +25,11 @@ def predict_knn(unnormalized_data, normalized_train, normalized_test, k, target_
 
     for tc in target_columns:
 
+        if tc in numeric_columns:
+            unnormalized_data[tc+"_prediction"] = 0
+        else:
+            unnormalized_data[tc + "_prediction"] = ""
+
         # print "\n\n\n\n##### Predict label:", tc
         correct_predict = 0
         abs_err_predict = 0
@@ -42,6 +47,7 @@ def predict_knn(unnormalized_data, normalized_train, normalized_test, k, target_
                 abs_err = abs(actual - median)
                 abs_err_predict = abs_err_predict + abs_err
                 # print "# Median:", median, "Error:", abs_err
+                unnormalized_data.set_value(index_test, tc + "_prediction", median)
             else:
                 value_counts = neighbor_values[tc].value_counts()
                 if len(value_counts) > 0:
@@ -51,12 +57,15 @@ def predict_knn(unnormalized_data, normalized_train, normalized_test, k, target_
                 if majority == actual:
                     correct_predict = correct_predict + 1
                 # print "Majority voting:", majority
+                unnormalized_data.set_value(index_test, tc + "_prediction", majority)
 
         if tc in numeric_columns:
             print "### Abs Error:", abs_err_predict / float(normalized_test.shape[0])
         else:
             print "### Correctly predicted:", correct_predict, "in %:", float(correct_predict) / float(normalized_test.shape[0])
         print "### Number of test rows:", normalized_test.shape[0]
+
+        return unnormalized_data
 
 
 def test_knn(file_name):
@@ -68,24 +77,25 @@ def test_knn(file_name):
     :type method: str
     """
 
-    data_frame = prepare_data(file_name, budget_name="")
-    data_frame_original = get_overall_job_reviews(data_frame.copy())
+    data_frame = prepare_data(file_name)
+    data_frame_original = data_frame.copy()
 
     df_train, df_test = train_test_split(data_frame, train_size=0.8)
 
     # in app, train data would be prepared in db
     df_train, min, max, vectorizers = prepare_data_clustering(df_train, z_score_norm=False, add_text=True)
 
+    # # balance data set for experience_level or job_type
+    # df_test = balance_data_set(df_test, "job_type", relative_sampling=False)
 
-
-    # balance data set for experience_level or job_type
-    df_test = balance_data_set(df_test, "job_type", relative_sampling=False)
-
-    # # remove rows without budget to predict budget
-    # df_test.ix[data_frame.budget == 0, 'budget'] = None
-    # df_test.dropna(subset=["budget"], how='any', inplace=True)
+    # remove rows without budget to predict_comparison budget
+    df_test.ix[df_test.job_type == 'Hourly', 'budget'] = None
+    df_test.dropna(subset=["budget"], how='any', inplace=True)
 
     # prepare test data
-    df_test = prepare_test_data(df_test, df_train.columns, min, max, vectorizers=vectorizers, weighting=True)
+    df_test = prepare_test_data_clustering(df_test, df_train.columns, min, max, vectorizers=vectorizers, weighting=True)
 
-    predict_knn(data_frame_original.copy(), df_train.copy(), df_test.copy(), k=15, target_columns=['job_type'])
+    unnormalized_data = predict_knn(data_frame_original.copy(), df_train.copy(), df_test.copy(), k=15, target_columns=['budget'])
+
+    print "\n"
+    evaluate_regression(unnormalized_data['budget'], unnormalized_data['budget_prediction'], 'budget')
